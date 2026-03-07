d1c8d79eeb322c30722b4a6e65f50a3185a174aa
Rajganguly2 <157829396+Rajganguly2@users.noreply.github.com>
2026-03-08 00:27:45 +0530

    update cors

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ require("dotenv").config();
 const { initDB } = require("./db");
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://docapp-frontend-5k4q-95g2w1yp8-rajdeep-gangulys-projects.vercel.app/"    // replace with your actual Vercel URL
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Routes
@@ -14,13 +21,13 @@ app.use("/api/doctors",      require("./routes/doctors"));
 app.use("/api/appointments", require("./routes/appointments"));
 app.use("/api/admin",        require("./routes/admin"));
 
-app.get("/", (req, res) => res.json({ status: "ok", message: "DocApp SQL API running" }));
+app.get("/", (req, res) => res.json({ status: "ok", message: "DocApp API running" }));
 
 // Init DB then start server
 initDB()
   .then(() => {
     app.listen(process.env.PORT || 5000, () =>
-      console.log(`🚀 Server running on http://localhost:${process.env.PORT || 5000}`)
+      console.log(`🚀 Server running on port ${process.env.PORT || 5000}`)
     );
   })
   .catch(err => {
